4ee382bf42ff5a5ae000c5554dcf5cffdfdc6dcc
MattHerrera7 <107372319+MattHerrera7@users.noreply.github.com>
2022-10-31 19:58:11 -0600

    Add files via upload
    Deliverable 1  update

---
+++ Schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE property_locations (
+	id integer,
+	airbnb_city text,
+	lat text,
+	lon integer,
+	star_rating integer
+	);
+	
+CREATE TABLE success_indicators(
+	id integer,
+	nights_booked integer,
+	occupancy integer,
+	rental_income integer,
+	star_rating integer
+	);
+CREATE TABLE properties(
+	id integer,
+	nights_booked integer,
+	occupancy integer,
+	rental_income integer,
+	star_rating integer
+	);
